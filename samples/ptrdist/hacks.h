564b805d3b64bfbcdad66f0d30b7ea0addea61fa
mwhicks1 <mwh@correctcomputation.com>
2021-04-23 10:48:49 -0400

    Revert

--- samples/ptrdist/hacks.h
+++ samples/ptrdist/hacks.h
@@ -1,6 +1,6 @@
 #define UncheckedPtrInit(type, variable, init) \
   type variable = 0; \
   _Unchecked { variable = init; }
-#define UncheckedBoundsInit(type, variable, b, init) \
-  type variable : b = 0; \
+#define UncheckedBoundsInit(type, variable, bounds, init) \
+  type variable : bounds = 0; \
   _Unchecked { variable = init; }
