f064c17c0d637739187230f5b7b6a72b2090d9b9
mwhicks1 <mwh@correctcomputation.com>
2021-04-25 16:46:11 -0400

    Olden health

---
+++ samples/Olden/health/health.h
@@ -0,0 +1,104 @@
+/* For copyright information, see olden_v1.0/COPYRIGHT */
+
+/****************************************************************
+ * health.h: header file for health.c                           *
+ ****************************************************************/
+
+
+#ifndef _HEALTH
+#define _HEALTH
+
+#include <stdchecked.h>
+#include <stdio.h>
+#include <stdlib.h>
+#pragma CHECKED_SCOPE ON
+
+#define chatting(...) unchecked { printf(__VA_ARGS__); }
+
+#define IA 16807
+#define IM 2147483647
+#define AM (1.0 / IM)
+#define IQ 127773
+#define IR 2836
+#define MASK 123459876
+
+extern int  max_level;
+extern long max_time;
+extern long long seed;
+
+struct Results {
+  float                   total_patients;
+  float                   total_time;
+  float                   total_hosps; 
+};
+
+struct Patient {
+  int                     hosps_visited;
+  int                     time;
+  int                     time_left;
+  ptr<struct Village>     home_village;
+};
+
+struct List {
+  ptr<struct List>        forward;
+  ptr<struct Patient>     patient;
+  ptr<struct List>        back;
+};
+
+struct Hosp {
+#if 1
+  int                    personnel; 
+  int                    free_personnel; 
+  int                    num_waiting_patients; 
+  struct List            waiting; 
+  struct List            assess; 
+  struct List            inside;
+  struct List            up;
+#else
+  int                    free_personnel; 
+  struct List            waiting;
+  struct List            assess;
+  struct List            inside;
+  struct List            up;
+  int                    personnel; 
+  int                    num_waiting_patients; 
+#endif
+};
+ 
+struct Village {
+#if 1
+  ptr<struct Village>    forward checked[4];
+  ptr<struct Village>    back;
+  struct List            returned;
+  struct Hosp            hosp;   
+  int                    label;
+  long long              seed;
+#else
+  struct Hosp            hosp;   
+  long                   seed;
+  ptr<struct Village>    forward checked[4];
+  int                    label;
+  struct List            returned;
+  ptr<struct Village>    back;
+#endif
+};
+
+ptr<struct Village> alloc_tree(int level, int label, ptr<struct Village> back);
+void dealwithargs(int argc, array_ptr<nt_array_ptr<char>> argv : count(argc));
+float my_rand(long long idum);
+ptr<struct Patient> generate_patient(ptr<struct Village> village);
+void put_in_hosp(ptr<struct Hosp> hosp, ptr<struct Patient>patient);
+void addList(ptr<struct List> list, ptr<struct Patient> patient);
+void removeList(ptr<struct List> list, ptr<struct Patient> patient);
+ptr<struct List> sim(ptr<struct Village> village);
+void check_patients_inside(ptr<struct Village> village, ptr<struct List> list);
+ptr<struct List> check_patients_assess(ptr<struct Village> village, ptr<struct List> list);
+void check_patients_waiting(ptr<struct Village> village, ptr<struct List> list);
+float get_num_people(ptr<struct Village> village);
+float get_total_time(ptr<struct Village> village);
+float get_total_hosps(ptr<struct Village> village);
+struct Results get_results(ptr<struct Village> village);
+
+#pragma CHECKED_SCOPE OFF
+#endif
+
---
+++ samples/Olden/health/list.c
@@ -0,0 +1,48 @@
+/* For copyright information, see olden_v1.0/COPYRIGHT */
+
+/********************************************************************
+ *  List.c:  Handles lists.                                         *
+ *           To be used with health.c                               *
+ ********************************************************************/
+
+#include <stdchecked.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "health.h"
+#pragma CHECKED_SCOPE ON
+
+void addList(ptr<struct List> list, ptr<struct Patient> patient) {
+  ptr<struct List> b = NULL;
+
+  while (list != NULL) {
+    b = list;
+    list = list->forward; }
+  
+  list = calloc<struct List>(1, sizeof(struct List));
+  list->patient = patient;
+  list->forward = NULL;
+  list->back = b;
+  b->forward = list;
+} 
+
+void removeList(ptr<struct List> list, ptr<struct Patient> patient) {
+  ptr<struct List>      l1 = NULL;
+  ptr<struct List>      l2 = NULL;
+  ptr<struct Patient>   p = list->patient;
+
+  while(p != patient) {
+      list = list->forward; 
+      p = list->patient;
+  }
+    
+  l1 = list->back;
+  l2 = list->forward;
+  l1->forward = l2;
+  if (list->forward != NULL) {
+    l1 = list->forward;
+    l2 = list->back;
+    l1->back = l2;
+  }
+  /*free(list);*/
+}
+     
