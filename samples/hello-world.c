096832343c387b49bd1ed24a034204c7c36bd142
mwhicks1 <mwh@correctcomputation.com>
2021-04-25 21:06:21 -0400

    Tidying

--- samples/hello-world.c
+++ samples/hello-world.c
@@ -1,28 +1,15 @@
-#include <stdio_checked.h>
+#include <stdio.h>
 #include <stdchecked.h>
 
-
-int main(int argc, char* argv[]) {
-
+int main(int argc, char** argv : itype(_Array_ptr<_Nt_array_ptr<char>>) count(argc)) {
   checked{
-
     int arr checked[5]  = { 0, 1, 2, 3, 4 };
-
     ptr<ptr<int>> q;
-
   }
 
-
   int arr checked[5]  = { 0, 1, 2, 3, 4 };
-
   ptr<ptr<int>> q;
-
   array_ptr <int > current : bounds (start , end ) = start;
-
-  array_ptr <int > current where current : bounds (start , end ) = start ;
-
   dynamic_check(1 == 1);
-
-  puts("hello, world");
   return 0;
 }
