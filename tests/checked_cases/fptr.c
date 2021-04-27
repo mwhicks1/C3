f92cc034a9367a873a8f83209105b0397ffba9f1
mwhicks1 <mwh@correctcomputation.com>
2021-04-27 17:23:16 -0400

    compile

--- tests/checked_cases/fptr.c
+++ tests/checked_cases/fptr.c
@@ -2,8 +2,8 @@ void foo(void) {
   _Ptr<int(unsigned int)> mapfunc = 0;
   _Ptr<int(int)> mapfunc2 = 0;
   _Ptr<int(volatile unsigned int)> mapfunc3 = 0;
-  _Ptr<int(volatile unsigned int)> mapfunc3 = 0;
-  _Ptr<const int(volatile unsigned int)> mapfunc3 = 0;
+  _Ptr<int(volatile unsigned int)> mapfunc4 = 0;
+  _Ptr<const int(volatile unsigned int)> mapfunc5 = 0;
   // should translate to 
   // int (*mapfunc)(unsigned int) = 0;
 }
