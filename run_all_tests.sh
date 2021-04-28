0610bf0bb4c71269e7f17f263910ea5c73bb5d60
Matt McCutchen (Correct Computation) <matt@correctcomputation.com>
2021-04-28 18:45:07 -0400

    run_all_tests.sh: Keep running tests after one fails.

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -4,23 +4,24 @@ C3_DIR=$(pwd)
 
 set -e
 
-function fail() { 
-  if [ -z $LASTCASE ] 
-  then
-    echo All Tests Pass!
-  else
-    echo Test $LASTCASE Failed!
-  fi
-}
- 
-trap fail EXIT
+all_passed=true
 
 for test_case in $(ls ./tests/checked_cases/)
 do
   echo testing $test_case
   full=./tests/checked_cases/$test_case
   LASTCASE=$full
-  C3_DIR=. ./tests/test_checked.sh $full
+  if ! C3_DIR=. ./tests/test_checked.sh $full; then
+    echo Test $LASTCASE failed!
+    all_passed=false
+  fi
 done
 
-LASTCASE=
+if $all_passed; then
+  echo 'All Tests Pass!'
+else
+  echo 'At least one test failed (see above)!'
+fi
+
+# Set exit status.
+$all_passed
