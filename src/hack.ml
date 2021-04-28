dac987243dd39612224e47895b95fa38fb0d5bd4
mwhicks1 <mwh@correctcomputation.com>
2021-04-28 13:04:55 -0400

    Missing file

---
+++ src/hack.ml
@@ -0,0 +1,6 @@
+let fl () = Stdlib.flush stdout
+          
+let tyvars : string list ref = ref []
+let note_tyvar t = (* Printf.printf "noting %s\n" t; fl(); *) tyvars := t::!tyvars
+let is_tyvar t = (* Printf.printf "looking for %s\n" t; fl(); *) List.mem t !tyvars
+let clear_tyvars () = (* Printf.printf "clearing all vars\n"; fl(); *) tyvars := [] 
